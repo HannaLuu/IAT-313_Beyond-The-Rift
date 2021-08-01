5a184976b2c7d3d21f1d8b201c0fadb014da038d
HannaLuu <pokemonhanna@gmail.com>
2021-07-31 22:16:25 -0700

    TELEPORTING WORKS
    (there are a couple bugs but... I think it's fine... LOL)

---
+++ Beyond The Rift/Assets/Scripts/Player Scripts/TP_Clone.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Clone : MonoBehaviour
+{
+    public float speed = 20f;
+    public int teleportDistance = 5;
+    public bool colliding = false;
+
+    public Vector3 startPosition;
+    public Vector3 currentPosition;
+
+    bool isFlipped = false;
+
+    SpriteRenderer sprite;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        startPosition = transform.position;
+
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        colliding = true;
+        sprite.color = new Color(0.5f, 0, 0, 0.5f);
+    }
+
+    private void OnTriggerExit2D(Collider2D hitInfo)
+    {
+        colliding = false;
+        sprite.color = new Color(1, 1, 1, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPosition = transform.position;
+
+        if (Input.GetButtonUp("Fire2"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (currentPosition.x >= (startPosition.x + teleportDistance) && isFlipped == false)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            rb.velocity = -rb.velocity;
+            isFlipped = true;
+        }
+        if (currentPosition.x <= (startPosition.x - teleportDistance) && isFlipped == false)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            rb.velocity = -rb.velocity;
+            isFlipped = true;
+        }
+        if (isFlipped && currentPosition.x == startPosition.x)
+        {
+            transform.Rotate(0f, 180f, 0f);
+            rb.velocity = -rb.velocity;
+            isFlipped = false;
+        }
+    }
+}
---
+++ Beyond The Rift/Assets/Scripts/Player Scripts/Teleport.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform clonePoint;
+
+    public GameObject clonePrefab;
+
+    GameObject clone;
+
+    Vector3 teleportPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject tpClone = GameObject.FindGameObjectWithTag("TP-Clone_Player");
+
+        //if(tpClone)
+        //{
+        //teleportPos = tpClone.transform.position;
+        //teleportPos = clone.transform.position;
+        //}
+
+        if(clone != null)
+        {
+            teleportPos = clone.GetComponent<TP_Clone>().transform.position;
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SendClone();
+        }
+
+        if (Input.GetButtonDown("Fire3") && clone.GetComponent<TP_Clone>().colliding == false)
+        {
+            Teleport();
+        }
+
+        void SendClone()
+        {
+            clone = Instantiate(clonePrefab, clonePoint.position, clonePoint.rotation) as GameObject;
+        }
+
+        void Teleport()
+        {
+            transform.position = teleportPos;
+        }
+    }
+}
